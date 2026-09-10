ba53a7d33854dc13488e24cae073f85680ba3e6d
reecepike <reece020408@gmail.com>
2026-09-10 22:32:22 +0100

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: { bodySizeLimit: '1mb' } },
-};
+const nextConfig = {};
 
 export default nextConfig;
